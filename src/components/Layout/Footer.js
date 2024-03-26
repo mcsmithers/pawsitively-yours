a97cb7efa226349d784ac2ec71b1140ae83b8639
Christina Smithers <christina.smithers@gmail.com>
2024-03-26 16:53:03 -0500

    fixed header and styling footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,9 +1,10 @@
 // src/components/Footer.js
 import React from 'react';
+import '../../styles/Footer.css'
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className="footer">
             {/* Footer content */}
             <p>&copy; 2024 Pawsitively Yours</p>
         </footer>
