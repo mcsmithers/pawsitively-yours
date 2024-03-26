86341105eb5717a761ec2741125d2df0767dd838
Christina Smithers <christina.smithers@gmail.com>
2024-03-26 11:15:25 -0500

    fixed rendering duplication

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,13 @@
 // src/pages/HomePage.js
 import React from 'react';
-import Layout from '../components/Layout/Layout';
 
 const HomePage = () => {
     return (
-        <Layout>
+        <div>
             {/* Main content for the home page */}
-            <h1>Welcome to My Pawsitively Yours</h1>
+            <h1>Welcome to Pawsitively Yours</h1>
             <p>This is the home page content.</p>
-        </Layout>
+        </div>
     );
 }
 
